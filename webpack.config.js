656211f3f26d718fec3a2bd7ea94d56a01139414
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2019-02-03 19:42:54 +0100

    prepare project

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './tmp/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js'
+  },
+  devServer: {
+    index: 'default.html',
+    contentBase: path.join(__dirname, "."),
+    compress: true,
+    port: 8081
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      }
+    ]
+  }
+};
