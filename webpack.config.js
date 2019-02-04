429cf05970f92e699088b2e172146e283bcbb7a8
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2019-02-04 09:14:55 +0100

    change path from tmp to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './tmp/index.js',
+  entry: './src/index.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'public'),
